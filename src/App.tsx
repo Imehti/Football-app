36ae8e2406841c57c49a02fb98666484c707c61e
Mahdi <khajavim497@gmail.com>
2024-04-24 11:52:26 +0430

    fix year filter options

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Table } from "antd";
 import SearchLeague from "./components/SearchLeague";
 import useTableStanding from "./hooks/useTableStanding";
+import FilterYear from "./components/FilterYear";
 
 function App() {
   const { columns, data } = useTableStanding();
   return (
     <>
-      <div>
+      <div className="sm:grid grid-cols-3 m-4">
         <SearchLeague />
+        <FilterYear />
       </div>
+
       <Table columns={columns} dataSource={data} />
     </>
   );
--- src/components/FilterYear.tsx
+++ src/components/FilterYear.tsx
@@ -1,23 +1,59 @@
 import { AutoComplete } from "antd";
+import React,{ useState } from "react";
 import { useRecoilState } from "recoil";
+import FilterYearState from "../recoil/atom/FilterYearState";
 
-const FilterYear = () => {
-    const startYear = 2010;
-    const endYear = 2023;
+export interface YearType {
+  value: number | string;
+  label: number | string;
+}
+
+const FilterYear: React.FC = () => {
+  const [value, setValue] = useState<number | string>();
+  const [options, setOptions] = useRecoilState<YearType[]>(FilterYearState);
   
-    const yearOptions = [];
-    for (let year = startYear; year <= endYear; year++) {
-      yearOptions.push({
-        label: `${year}-${year + 1}`,
-        value: `${year}-${year + 1}`
-      });
+  const startYear = 2010;
+  const endYear = 2023;
+
+  const yearOptions: YearType[] = [];
+  for (let year = startYear; year <= endYear; year++) {
+    yearOptions.push({
+      label: `${year}-${year + 1}`,
+      value: `${year}-${year + 1}`
+    });
+  }
+
+  const onSelect = (data: number | string) => {
+    setValue(data);
+  };
+
+  const onChange = (data: number | string) => {
+    setValue(data);
+    if (data === '') {
+      setOptions(yearOptions);
+    } else {
+      const filteredOptions = yearOptions?.filter((option:any) =>
+        option.label.includes(data.toString())
+      );
+      setOptions(filteredOptions);
     }
- 
+  };
 
-    return(
-        <></>
-    )
-    
-}  
+  return (
+    <>
+      <div className="sm:flex items-center w-full">
+        <p className="mx-2">Year:</p>
+        <AutoComplete
+          className="w-1/3"
+          value={value}
+          options={yearOptions} // set options prop to yearOptions
+          onSelect={onSelect}
+          onChange={onChange}
+          placeholder="Search Year..."
+        />
+      </div>
+    </>
+  );
+};
 
-export default FilterYear
+export default FilterYear;
