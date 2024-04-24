4a3d2a2ae29da81279b7f663d74ff7d34d7bf704
Mahdi <khajavim497@gmail.com>
2024-04-24 10:50:01 +0430

    make year options

--- src/components/FilterYear.tsx
+++ src/components/FilterYear.tsx
@@ -1,2 +1,23 @@
 import { AutoComplete } from "antd";
 import { useRecoilState } from "recoil";
+
+const FilterYear = () => {
+    const startYear = 2010;
+    const endYear = 2023;
+  
+    const yearOptions = [];
+    for (let year = startYear; year <= endYear; year++) {
+      yearOptions.push({
+        label: `${year}-${year + 1}`,
+        value: `${year}-${year + 1}`
+      });
+    }
+ 
+
+    return(
+        <></>
+    )
+    
+}  
+
+export default FilterYear
