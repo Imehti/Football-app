121730968d3e8502677bb9eff7bc731e6f9a73ed
Mahdi <khajavim497@gmail.com>
2024-04-30 12:44:37 +0430

    get round value from autocomplete and fix the search year to show events

--- src/components/NextMatches.tsx
+++ src/components/NextMatches.tsx
@@ -5,13 +5,17 @@ import useTableStanding from "./TableStanding";
 import isSelectedLeagueaValue from "../recoil/selector/isSelectedLeagueValue";
 import FilterdYearValue from "../recoil/selector/FilterdYearValue";
 import isSelectedYearValue from "../recoil/selector/isSelectedYearValue";
+import FilterRound from "./FilterRound";
+import FilterdRoundValue from "../recoil/selector/FilterRoundValue";
 
 function NextMatches() {
   const leagueSearchedValue = useRecoilValue(searchedLeagueValue);
   const yearValue = useRecoilValue(FilterdYearValue);
   const selectedLeagueStatus = useRecoilValue(isSelectedLeagueaValue);
   const selectedYearStatus = useRecoilValue(isSelectedYearValue);
+  const roundValue = useRecoilValue(FilterdRoundValue);
 
+  console.log(roundValue);
 
   const { data: tableDetails } = useTableStanding();
   const maxPlayed = Math.max(...tableDetails.map((team) => team.intPlayed));
@@ -26,48 +30,60 @@ function NextMatches() {
   }
 
   console.log(nextEvents);
-  
 
   return (
     <>
-      <div className="flex justify-center font-bold">
-        {yearValue[0]?.value=='2023-2024'?<h1>NEXT EVENTS</h1> :<h1> EVENTS</h1>}
+      <div className="grid grid-cols-2 m-2 items-center font-bold">
+        <FilterRound />
+        {yearValue[0]?.value == "2023-2024" ? (
+          <h1>NEXT EVENTS</h1>
+        ) : (
+          <h1> EVENTS</h1>
+        )}
       </div>
       <div className="grid sm:grid-cols-4 grid-cols-2 gap-8 m-4">
-        {nextEvents?.events.map((event) => (
-          <>
-            {" "}
-            <div className="border border-gray-500 flex flex-col rounded p-2">
-              <div className="grid grid-cols-2 gap-8">
-                <div className="flex">
-                  <img
-                    className="w-8 h-8"
-                    src={event.strHomeTeamBadge}
-                    alt={event.strHomeTeam}
-                  />
-                  <span className="ml-1">{event.strHomeTeam}</span>
+        {nextEvents?.events &&
+          nextEvents?.events.map((event) => (
+            <>
+              {" "}
+              <div className="border border-gray-500 flex flex-col rounded p-2">
+                <div className="grid grid-cols-2 gap-8">
+                  <div className="flex">
+                    <img
+                      className="w-8 h-8"
+                      src={event.strHomeTeamBadge}
+                      alt={event.strHomeTeam}
+                    />
+                    <span className="ml-1">{event.strHomeTeam}</span>
+                  </div>
+                  <div>
+                    <span>{event.dateEvent}</span>
+                  </div>
                 </div>
-                <div>
-                  <span>{event.dateEvent}</span>
-                </div>
-              </div>
 
-              <div className="grid grid-cols-2 gap-8">
-                <div className="flex mt-2">
-                  <img
-                    className="w-8 h-8"
-                    src={event.strAwayTeamBadge}
-                    alt={event.strAwayTeam}
-                  />
-                  <span className="ml-1">{event.strAwayTeam}</span>
-                </div>
-                <div>
-                  <span>{event.strTime}</span>
+                <div className="grid grid-cols-2 gap-8">
+                  <div className="flex mt-2">
+                    <img
+                      className="w-8 h-8"
+                      src={event.strAwayTeamBadge}
+                      alt={event.strAwayTeam}
+                    />
+                    <span className="ml-1">{event.strAwayTeam}</span>
+                  </div>
+                  <div>
+                    <span>{event.strTime}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </>
-        ))}
+            </>
+          ))}
+      </div>
+      <div className="flex justify-center mb-2">
+        {!nextEvents?.events && (
+          <h1 className="font-bold text-red-700">
+            Please Select Season From 2012-2013
+          </h1>
+        )}
       </div>
     </>
   );
