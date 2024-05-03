2bad2c10b38eeec3aefff06e16561530939fc236
Mahdi <khajavim497@gmail.com>
2024-05-03 20:46:45 +0430

    create new component to use loading snip

---
+++ src/components/Loading.jsx
@@ -0,0 +1,16 @@
+
+import { Flex,Spin } from 'antd';
+
+function Loading(){
+    return(
+        <>
+         <Flex align="center" gap="middle">
+         <Spin size="small" />
+         <Spin />
+         <Spin size="large" />
+       </Flex>
+        </>
+    )
+}
+
+export default Loading
