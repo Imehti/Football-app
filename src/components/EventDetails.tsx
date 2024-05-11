f8b5f74b66e337b77e7d3f3580a6137ada66fefd
Mahdi <khajavim497@gmail.com>
2024-05-12 01:15:14 +0430

    refetch data when selected event is true

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -1,26 +1,32 @@
-import { useRecoilValue } from "recoil"
-import FileNameValue from "../recoil/selector/FileNameValue"
+import { useRecoilState, useRecoilValue } from "recoil";
+import FileNameValue from "../recoil/selector/FileNameValue";
 import useEventDetails from "../hooks/useEventDetails";
+import selectedEventStatus from "../recoil/selector/SelectedEventStatus";
+import Loading from "./Loading";
 
-function EventDetails(){
-    const fileName=useRecoilValue(FileNameValue)
+
+function EventDetails() {
+  const fileName = useRecoilValue(FileNameValue);
+  
+
+  const { data: eventDetails , refetch , isLoading} = useEventDetails(fileName.replace(/ /g, "_"));
+  const selectedEvent=useRecoilValue(selectedEventStatus)
 
 
- 
-    
 
-    const {data:eventDetails}=useEventDetails(fileName.replace(/ /g,"_"))
-    console.log(eventDetails?.event.map(e=>e.strFilename));
-    
-    
-    //fix the map
-return(
+if(selectedEvent){
+    refetch()
+
+}
+  
+  
+  return (
     <>
-    
-    <h1>{eventDetails?.event.map(match => match.strFilename)}</h1>
+    {isLoading && <Loading />}
+      <h1>{eventDetails?.event.map((match) => match.strFilename)}</h1>
+      <img src={eventDetails?.event.map((e) => e.strMap).join()} alt="" />
     </>
-)
+  );
 }
 
-
-export default EventDetails
+export default EventDetails;
