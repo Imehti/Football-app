a9bd47afe23c47cb3af7506cc39b078ef33380a0
Mahdi <khajavim497@gmail.com>
2024-05-12 00:34:17 +0430

    create a selector to know which event is selected

---
+++ src/recoil/selector/SelectedEventStatus.ts
@@ -0,0 +1,13 @@
+import { get } from "lodash";
+import { selector } from "recoil";
+import isSelectedEventState from "../atom/isSelectedEventState";
+
+const selectedEventStatus=selector({
+    key:'selectedEventState',
+    get:({get}) => {
+        const selectedEventValue=get(isSelectedEventState)
+        return selectedEventValue
+    }
+})
+
+export default selectedEventStatus
