31c649fb60bcff3bb70d17ecf10eef78d5fbcf5d
Mahdi <khajavim497@gmail.com>
2024-05-15 01:40:04 +0430

    design the UI and render it with some condition

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -5,6 +5,9 @@ import { useParams } from "react-router-dom";
 function EventDetails() {
   const params = useParams();
   const { data: eventDetails, isLoading } = useEventDetails(params.eventName);
+  console.log(eventDetails);
+
+  const videoUrl = eventDetails?.event.find((e) => e.strVideo)?.strVideo;
 
 
   if (isLoading) {
@@ -15,11 +18,47 @@ function EventDetails() {
     <>
       {eventDetails && (
         <>
-          <div className="flex justify-center mt-8 p-2">
-          <img src={eventDetails.event.map((e) => e.strBanner).join()} alt="" />
+          <div className="flex justify-center mt-4">
+            <img
+              className="rounded-xl sm:size-2/4"
+              src={eventDetails.event.map((e) => e.strThumb).join()}
+              alt=""
+            />
           </div>
-          <div className="flex justify-center items-center mt-4 border bg-blue-200">
-          <h1 className="font-bold sm:text-2xl">{eventDetails.event.map((e) => e.strEvent).join()}</h1>
+          <div className="flex flex-col mt-2 border bg-blue-200">
+            <div className="flex justify-center">
+              <h1 className="font-bold sm:text-2xl">
+                {eventDetails.event.map((e) => e.strEvent)}
+              </h1>
+            </div>
+            <div className="flex justify-center items-center">
+            <h1 className="font-bold sm:text-2xl">
+                {eventDetails.event.map((e) => {return <div className="flex justify-between">{e.intHomeScore} : {e.intAwayScore}</div>})}
+              </h1>
+            </div>
+            <div className="m-2 grid grid-cols-1 sm:grid-cols-2">
+            <div>
+            <h1 className="font-bold">
+                Season : {eventDetails.event.map((e) => e.strSeason)}
+              </h1>
+              <h1 className="font-bold">
+                Week : {eventDetails.event.map((e) => e.intRound)}
+              </h1>
+              <h1 className="font-bold">
+                On : {eventDetails.event.map((e) => {return <span>''{e.dateEvent}'' {e.strTime}</span>})} 
+              </h1>
+              {eventDetails.event?.some((e) => e.strVenue !== null) &&  <h1 className="font-bold">
+                  Stadium : {eventDetails.event.find((e) => e.strVenue !== null)?.strVenue} 
+                  </h1> }
+              {eventDetails.event?.some((e) => e.intSpectators !== null) &&  <h1 className="font-bold">
+                  Spectators : {eventDetails.event.find((e) => e.intSpectators !== null)?.intSpectators} 
+                  </h1> }
+                {eventDetails.event.some((e) => e.strDescriptionEN !== "" && e.strDescriptionEN !== null) &&  <h1 className="font-bold">
+                  Description : {eventDetails.event.find((e) => e.strDescriptionEN !== "")?.strDescriptionEN} 
+                  </h1> }
+            </div>
+              {videoUrl && <video src={videoUrl} controls></video>}
+            </div>
           </div>
         </>
       )}
